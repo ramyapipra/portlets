5f0b2079acbdaf66941ee94c2b91248068c76a71
ramyapipra <86221574+ramyapipra@users.noreply.github.com>
2021-11-12 12:58:31 +0530

    path change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
-import Dashboard from './dashboard/Dashboard';
+import Dashboard from './Dashboard/dashboard';
 const AppRouter = () => {
   let routes = useRoutes([
     { path: "/", element: <Dashboard /> },
